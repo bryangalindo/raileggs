688fb746b09cdbadf32982ee52d72770a51a9082
Bryan Galindo <galindo.bryan08@gmail.com>
2019-06-29 19:23:08 -0500

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,30 @@
+bnsf_data = {
+    'selectedValues': '',
+    'spoolId': '',
+    'cmd': '',
+    'patName': '',
+    'patAddress': '',
+    'patAddress2': '',
+    'patCity': '',
+    'patState': '',
+    'patAttn': '',
+    'patZip': '',
+    'patPhone': '',
+    'selectStation': '',
+    'hEqpInit': '',
+    'hEqpNumb': '',
+    'selTotal': '',
+    'equipment': '{},',
+}
+
+bnsf_containers_list = [
+    'TCKU3833402',
+    'TCLU2702730',
+    'GLDU3646607',
+    'OOLU9600981',
+]
+
+uprr_scrape_url = "https://c02.my.uprr.com/wet-service/secure/jas/1_3/trace?equipmentIds={}"
+uprr_request_token_url = "https://c02.my.uprr.com/api/oauth/token"
+username = "hi"
+password = "hello"
