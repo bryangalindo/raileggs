85cc031f110d1b092246b71b3671b07c29c1d80d
Bryan Galindo <galindo.bryan08@gmail.com>
2019-08-18 20:21:40 -0500

    cleaned up code

--- cn_rail.py
+++ cn_rail.py
@@ -33,7 +33,7 @@ class CanadianRail:
         load_status_key = _dict['location_html'][index].split()[6]
         return load_status_dict[load_status_key]
 
-    def get_current_event(self, index, _dict):
+    def get_recent_event(self, index, _dict):
         now = datetime.now()
         rail_status_key = _dict['location_html'][index].split()[7]
         event_datetime_str = self.remove_at(4, (''.join(_dict['location_html'][index].split()[4:6])))
@@ -41,10 +41,10 @@ class CanadianRail:
         event_datetime = datetime.strftime(raw_event_datetime, '%m/%d/{} %H:%M'.format(now.year))
         return dict(most_recent_event=cn_rail_events_dict[rail_status_key], datetime=event_datetime)
 
-    def get_current_location(self, index, _dict):
+    def get_recent_location(self, index, _dict):
         location_list = _dict['location_html'][index].split()[2:4]
         current_location = ', '.join(location_list)
-        return dict(current_location=current_location)
+        return dict(most_recent_location=current_location)
 
     def get_next_destination(self, index, _dict):
         location_list = _dict['location_html'][index].split()[9:11]
@@ -54,12 +54,32 @@ class CanadianRail:
     def get_final_eta(self, index, _dict):
         try:
             now = datetime.now()
-            final_eta_str = _dict['eta_html'][index].split()[-1]
-            final_eta_datetime = datetime.strptime(final_eta_str[:4], '%m%d')
-            final_eta = datetime.strftime(final_eta_datetime, '%m/%d/{}'.format(now.year))
-            return dict(final_eta=final_eta)
+            tracing_result_list = _dict['eta_html'][index].split()
+            if len(tracing_result_list) == 5:
+                final_eta_str = tracing_result_list[-2]
+                final_eta_datetime = datetime.strptime(final_eta_str[2:6], '%m%d')
+                final_eta = datetime.strftime(final_eta_datetime, '%m/%d/{}'.format(now.year))
+                return dict(final_eta=final_eta)
+            elif len(tracing_result_list) == 4:
+                final_eta_str = tracing_result_list[-2]
+                final_eta_datetime = datetime.strptime(final_eta_str[11:15], '%m%d')
+                final_eta = datetime.strftime(final_eta_datetime, '%m/%d/{}'.format(now.year))
+                return dict(final_eta=final_eta)
+            else:
+                final_eta_str = tracing_result_list[-1]
+                final_eta_datetime = datetime.strptime(final_eta_str[:4], '%m%d')
+                final_eta = datetime.strftime(final_eta_datetime, '%m/%d/{}'.format(now.year))
+                return dict(final_eta=final_eta)
         except ValueError:  # catches strptime trying to turn letters into datetime object
-            pass
+            print('Check if container tracing results split list is different length')
 
     def remove_at(self, index, s):
         return s[:index] + s[index + 1:]
+
+    def get_all_tracing_results(self, index, _dict):
+        return {
+            self.get_recent_location(index, _dict),
+            self.get_next_destination(index, _dict),
+            self.get_final_eta(index, _dict),
+            self.get_recent_event(index, _dict),
+        }
