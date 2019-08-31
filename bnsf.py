9bc61489ecc3de0c278d394797701c669924d544
Bryan Galindo <galindo.bryan08@gmail.com>
2019-08-30 22:36:05 -0500

    added init func

--- bnsf.py
+++ bnsf.py
@@ -4,25 +4,30 @@ from bs4 import BeautifulSoup
 
 
 class BNSF:
+    def __init__(self, containers_list):
+        self.containers_list = containers_list
 
-    def get_tracing_results_html(self, container_list):
+    def format_containers(self):
         formatted_containers_list = []
-        for container in container_list:
+        for container in self.containers_list:
             if '0' in container[4]:
                 formatted_container_str = container[:4] + container[5:-1]
                 formatted_containers_list.append(formatted_container_str)
             else:
                 formatted_containers_list.append(container[:-1])
+        return formatted_containers_list
 
+    def get_tracing_results_html(self):
+        formatted_containers_list = self.format_containers()
         bnsf_data['equipment'] = ','.join(formatted_containers_list)
         response = requests.post(bnsf_scrape_url, data=bnsf_data)
         html = BeautifulSoup(response.content, 'html.parser')
         container_tags = html.find_all('tr', {'id': 'dllRowStyle'})
         return container_tags
 
-    def get_tracing_results_dict(self, container_list):
+    def get_tracing_results_dict(self):
         tracing_results_list = []
-        container_tags = self.get_tracing_results_html(container_list)
+        container_tags = self.get_tracing_results_html()
         for tag in container_tags:
             tracing_results_list.append(dict(
                 container_number='{}{}'.format((tag.find('td', {'id': 'UnitInit'})).text,
