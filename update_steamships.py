d9b87b2ba25d2abd4bcd6166186cb34e9e1e3a2c
Bryan Galindo <galindo.bryan08@gmail.com>
2019-08-31 18:15:00 -0500

    uncommented blocks

--- update_steamships.py
+++ update_steamships.py
@@ -11,40 +11,40 @@ from database import get_containers_by_steamship, update_container_eta, update_c
 
 session = requests.Session()
 
-# apl = apl.APL(session)
-# tracing_results_list = []
-# apl_containers_list = get_containers_by_steamship()['APLU'] + get_containers_by_steamship()['CMDU']
-#
-# pool = multiprocessing.Pool(processes=len(apl_containers_list))
-# with pool as p:
-#     tracing_results = p.map(apl.get_tracing_results_dict, apl_containers_list)
-#     for result in tracing_results:
-#         tracing_results_list.append(result)
-#
-# for result in tracing_results_list:
-#     formatted_tracing_results = apl.format_tracing_results(result)
-#     update_container_eta(result['container'], result['vessel_eta'])
-#     update_container_tracing(result['container'], formatted_tracing_results, 'ssl')
-#
-#
-# msc = msc.MSC(session)
-# msc_containers_list = get_containers_by_steamship()['MEDU']
-#
-# for container in msc_containers_list:
-#     html = msc.get_tracing_results_html(container)
-#     update_container_tracing(container, msc.get_all_events(html), 'ssl')
-#     update_container_eta(container, msc.get_vessel_eta(html))
-#
-# hpl = hpl.HPL(session)
-# hapag_containers_list = get_containers_by_steamship()['HLCU']
-#
-# for container in hapag_containers_list:
-#     try:
-#         html = hpl.get_tracing_results_html(container)
-#         update_container_tracing(container, hpl.get_all_events(html), 'ssl')
-#         update_container_eta(container, hpl.get_vessel_eta(html))
-#     except AttributeError:
-#         pass
+apl = apl.APL(session)
+tracing_results_list = []
+apl_containers_list = get_containers_by_steamship()['APLU'] + get_containers_by_steamship()['CMDU']
+
+pool = multiprocessing.Pool(processes=len(apl_containers_list))
+with pool as p:
+    tracing_results = p.map(apl.get_tracing_results_dict, apl_containers_list)
+    for result in tracing_results:
+        tracing_results_list.append(result)
+
+for result in tracing_results_list:
+    formatted_tracing_results = apl.format_tracing_results(result)
+    update_container_eta(result['container'], result['vessel_eta'])
+    update_container_tracing(result['container'], formatted_tracing_results, 'ssl')
+
+
+msc = msc.MSC(session)
+msc_containers_list = get_containers_by_steamship()['MEDU']
+
+for container in msc_containers_list:
+    html = msc.get_tracing_results_html(container)
+    update_container_tracing(container, msc.get_all_events(html), 'ssl')
+    update_container_eta(container, msc.get_vessel_eta(html))
+
+hpl = hpl.HPL(session)
+hapag_containers_list = get_containers_by_steamship()['HLCU']
+
+for container in hapag_containers_list:
+    try:
+        html = hpl.get_tracing_results_html(container)
+        update_container_tracing(container, hpl.get_all_events(html), 'ssl')
+        update_container_eta(container, hpl.get_vessel_eta(html))
+    except AttributeError:
+        pass
 
 timestamp = int(time.time())
 cosco_containers_list = get_containers_by_steamship()['COSU']
