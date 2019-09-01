4471720ac24228b704c6335497eaed07af645f74
Bryan Galindo <galindo.bryan08@gmail.com>
2019-08-31 19:44:15 -0500

    added one line tracking

--- update_steamships.py
+++ update_steamships.py
@@ -5,12 +5,17 @@ import requests
 
 import apl
 import cosco
+from database import get_containers_by_steamship, update_container_eta, update_container_tracing
 import hpl
 import msc
-from database import get_containers_by_steamship, update_container_eta, update_container_tracing
+import one_line
+
 
 session = requests.Session()
 
+'''
+APL TRACKING
+'''
 apl = apl.APL(session)
 tracing_results_list = []
 apl_containers_list = get_containers_by_steamship()['APLU'] + get_containers_by_steamship()['CMDU']
@@ -26,7 +31,9 @@ for result in tracing_results_list:
     update_container_eta(result['container'], result['vessel_eta'])
     update_container_tracing(result['container'], formatted_tracing_results, 'ssl')
 
-
+'''
+MSC TRACKING
+'''
 msc = msc.MSC(session)
 msc_containers_list = get_containers_by_steamship()['MEDU']
 
@@ -35,6 +42,9 @@ for container in msc_containers_list:
     update_container_tracing(container, msc.get_all_events(html), 'ssl')
     update_container_eta(container, msc.get_vessel_eta(html))
 
+'''
+HAPAG TRACKING
+'''
 hpl = hpl.HPL(session)
 hapag_containers_list = get_containers_by_steamship()['HLCU']
 
@@ -46,6 +56,9 @@ for container in hapag_containers_list:
     except AttributeError:
         pass
 
+'''
+COSCO TRACKING
+'''
 timestamp = int(time.time())
 cosco_containers_list = get_containers_by_steamship()['COSU']
 
@@ -59,3 +72,17 @@ for event in most_recent_events_list:
     vessel_eta = cosco.get_vessel_eta(container_number)
     update_container_eta(container_number, vessel_eta)
     update_container_tracing(container_number, cosco.format_most_recent_event(event), 'ssl')
+
+'''
+ONE LINE TRACKING
+'''
+one = one_line.ONE(session)
+one_containers_list = get_containers_by_steamship()['ONEY']
+
+for container in one_containers_list:
+    events_dict = one.get_events_dict(container)
+    tracing_results = one.get_formatted_events(events_dict)
+    vessel_eta = one.get_vessel_eta(container)
+    update_container_eta(container, vessel_eta)
+    update_container_tracing(container, tracing_results, 'ssl')
+
