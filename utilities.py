30c82be15904de0ac4b6c87545b9f9a9d2929dff
Bryan Galindo <galindo.bryan08@gmail.com>
2019-09-05 18:36:57 -0500

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -1,3 +1,12 @@
+import selenium.webdriver as webdriver
+from constants import driver_path
+
 
 def has_digits(input_str):
     return any(char.isdigit() for char in input_str)
+
+
+def start_headless_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    return webdriver.Chrome(executable_path=driver_path)
